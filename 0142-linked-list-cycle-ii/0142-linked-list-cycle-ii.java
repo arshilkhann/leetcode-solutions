cea70ac5e20b4e19d4f0c78a4408961af2af84a4
arshilkhann__ <arshilkhan529@gmail.com>
2026-08-22 23:44:18 +0530

    Time: 0 ms (100%), Space: 46.8 MB (30.69%) - LeetHub

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.java
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.java
@@ -20,7 +20,6 @@ public class Solution {
 
             if(slow == fast){
                 slow = head;
-
                 while(slow != fast){
                     slow = slow.next;
                     fast = fast.next;
