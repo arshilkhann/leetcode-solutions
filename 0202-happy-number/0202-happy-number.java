31299fdc844a3636973678d8806db9bb209ac729
arshilkhann__ <arshilkhan529@gmail.com>
2026-08-22 23:54:37 +0530

    Time: 1 ms (78.16%), Space: 42.2 MB (79.37%) - LeetHub

--- 0202-happy-number/0202-happy-number.java
+++ 0202-happy-number/0202-happy-number.java
@@ -6,17 +6,17 @@ class Solution {
         while(fast != 1){
             slow = sumDigit(slow);
             fast = sumDigit(sumDigit(fast));
-
-            if(slow == fast && slow != 1){
+        
+            if(slow == fast && slow!= 1){
                 return false;
             }
         }
         return true;
     }
-    public int sumDigit(int n){
-        int sum = 0;
 
-        while( n > 0){
+    public int sumDigit(int n ){
+        int sum = 0;
+        while(n >0){
             int d = n % 10;
             n = n / 10;
             sum += d*d;
