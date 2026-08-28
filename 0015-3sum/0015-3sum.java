0579d65fa67210978d996ecd0ac0ad268cc94855
arshilkhann__ <arshilkhan529@gmail.com>
2026-08-28 21:29:03 +0530

    Time: 33 ms (72.08%), Space: 59 MB (86.06%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,37 +1,37 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        List<List<Integer>> result = new ArrayList<>();
         Arrays.sort(nums);
+        List<List<Integer>> list = new ArrayList<>();
 
-        for(int i = 0 ; i<nums.length; i++){
+        for(int i = 0; i < nums.length-2 ; i++){
             int left = i+1;
             int right = nums.length-1;
 
-            if(i>0 && nums[i] == nums[i-1]){
+            if(i > 0 && nums[i]== nums[i-1]){
                 continue;
             }
-            while(left<right){
-                if(nums[i] + nums[left] + nums[right] == 0){
-                    result.add(Arrays.asList(nums[i], nums[left], nums[right]));
-
+            while(left < right){
+                int total = nums[i]+nums[left]+nums[right];
+                if(total == 0){
+                    list.add(Arrays.asList(nums[i],nums[left],nums[right]));
                     left++;
                     right--;
-
-                    while(left<right && nums[left] == nums[left-1]){
+                
+                    while(left < right && nums[left] == nums[left-1]){
                         left++;
                     }
-                    while(left<right && nums[right] == nums[right+1]){
+                    while(left < right && nums[right] == nums[right+1]){
                         right--;
                     }
                 }
-                else if(nums[i] + nums[left] + nums[right] < 0){
+                else if(total < 0){
                     left++;
                 }
-                else{
+                else {
                     right--;
                 }
             }
         }
-        return result;
+        return list;
     }
 }
