65e9b2b8fd80c697962ccff050a326e1f459c117
arshilkhann__ <arshilkhan529@gmail.com>
2026-09-26 21:57:48 +0530

    Time: 6 ms (74.4%), Space: 66.7 MB (86.47%) - LeetHub

--- 1749-maximum-absolute-sum-of-any-subarray/1749-maximum-absolute-sum-of-any-subarray.java
+++ 1749-maximum-absolute-sum-of-any-subarray/1749-maximum-absolute-sum-of-any-subarray.java
@@ -1,34 +1,20 @@
 class Solution {
+    public int maxAbsoluteSum(int[] nums) {
+        int maxSum = 0;
+        int minSum = 0;
 
-    static int maxSumSubArray(int[] a){
-        int maxSum = a[0];
-        int best = a[0];
-        for(int i=1 ; i < a.length ; i++){
-            best = Math.max(best + a[i],a[i]);
-            maxSum = Math.max(maxSum , best);
+        int currentMax = 0;
+        int currentMin = 0;
+
+        for (int num : nums) {
+
+            currentMax = Math.max(0, currentMax + num);
+            maxSum = Math.max(maxSum, currentMax);
+
+            currentMin = Math.min(0, currentMin + num);
+            minSum = Math.min(minSum, currentMin);
         }
-        return Math.abs(maxSum);
-    }
 
-    static int minSumSubArray(int[] a){
-        int minSum = a[0];
-        int best = a[0];
-        for(int i=1 ; i < a.length ; i++){
-            best = Math.min(best + a[i],a[i]);
-            minSum = Math.min(minSum , best);
-        }
-        return Math.abs(minSum);
-    }
-
-    public int maxAbsoluteSum(int[] a) {
-        int res = 0;
-        int best = a[0];
-
-        int min = minSumSubArray(a);
-        int max = maxSumSubArray(a);
-        
-        res = Math.max(min,max);
-
-        return res ;
+        return Math.max(maxSum, Math.abs(minSum));
     }
 }
